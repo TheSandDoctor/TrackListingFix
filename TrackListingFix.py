f1309a6838d74b606b7e35fe3f7205d216672493
TheSandDoctor <33101968+TheSandDoctor@users.noreply.github.com>
2018-03-07 12:07:41 -0800

    Update TrackListingFix.py

--- TrackListingFix.py
+++ TrackListingFix.py
@@ -1,21 +1,40 @@
-import mwclient, ConfigParser
+#!/usr/bin/env python3
+import mwclient, configparser
 import mwparserfromhell
+#import sys
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
 def main():
+    from sys import argv
+    myargs = getopts(argv)
+    dry_run = False
+    if '-dry-run' in myargs:  # Example usage.
+        print(myargs['-dry-run'])
+        dry_run = True
+    print(myargs)
     site = mwclient.Site(('https','en.wikipedia.org'), '/w/')
-    config = ConfigParser.RawConfigParser()
-    config.read('credentials.txt')
+    #config = configparser.RawConfigParser()
+    #config.read('credentials.txt')
     #TODO: site.login(config.get('enwiki','username'), config.get('enwiki', 'password'))
     page = site.Pages['0 to 1 no Aida']#'3 (Bo Bice album)']
     text = page.text()
     wikicode = mwparserfromhell.parse(text)
     templates = wikicode.filter_templates()
-    #for template in templates:
-     # print("Found template %s" % template.name)
-      #for param in template.params:
-        # print("\tFound param %s with value %s" % (param.name, param.value))
-    text_file = open("Input.txt","w")
-    text_file.write(text)
-    text_file.close()
+
+    #TODO: Testing (dry run) only
+    if dry_run:
+        text_file = open("Input.txt","w")
+        text_file.write(text)
+        text_file.close()
+    #TODO: End dry run only
+
     code = mwparserfromhell.parse(text)
     for template in code.filter_templates():#Tracklist, Track, Soundtrack, Tlist, Track list
         template.name = template.name.lower()
@@ -32,9 +51,13 @@ def main():
                 template.remove("music_credits",False)
                 print("Removed music_credits")
     text = str(code)
-    text_file = open("Output.txt", "w")
-    text_file.write(text)
-    text_file.close()
-    
+    #TODO: save page here with summary
+    if dry_run:
+        print("DRY")
+        text_file = open("Output.txt", "w")
+        text_file.write(text)
+        text_file.close()
+    else:
+        print("REAL")
 if __name__ == "__main__":
     main()
