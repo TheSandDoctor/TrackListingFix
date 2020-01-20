66b822a515c286a4cd67eb6b7373b204ae9022f8
TheSandDoctor <majorjohn1@mail.com>
2020-01-19 22:49:18 -0800

    Code formatting/import cleanup
    Formatted TrackListingFix.py and added custom mwparserfromhell repo as submodule.

--- TrackListingFix.py
+++ TrackListingFix.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3.6
-import mwclient, configparser, mwparserfromhell, re, argparse, sys
-from mwclient import *
 import json
+
+import argparse
+import configparser
+import mwclient
+import mwparserfromhell
+import sys
+from mwclient import *
+
 
 def call_home(site):
     page = site.Pages['User:DeprecatedFixerBot/status']
@@ -10,6 +16,8 @@ def call_home(site):
     if str(data) == str(True):
         return True
     return False
+
+
 def allow_bots(text, user):
     user = user.lower().strip()
     text = mwparserfromhell.parse(text)
@@ -21,16 +29,20 @@ def allow_bots(text, user):
     for param in tl.params:
         bots = [x.lower().strip() for x in param.value.split(",")]
         if param.name == 'allow':
-            if ''.join(bots) == 'none': return False
+            if ''.join(bots) == 'none':
+                return False
             for bot in bots:
                 if bot in (user, 'all'):
                     return True
         elif param.name == 'deny':
-            if ''.join(bots) == 'none': return True
+            if ''.join(bots) == 'none':
+                return True
             for bot in bots:
                 if bot in (user, 'all'):
                     return False
     return True
+
+
 def get_next_iter_item(some_iterable, window=1):
     """
     Get the item that will be in next iteration of the loop.
@@ -43,49 +55,52 @@ def get_next_iter_item(some_iterable, window=1):
     items, nexts = tee(some_iterable, 2)
     nexts = islice(nexts, window, None)
     return zip_longest(items, nexts)
+
+
 def save_edit(page, utils, text):
-     config = utils[0]
-     #api = utils[1]
-     site = utils[2]
-     dry_run = utils[4]
-     original_text = text
+    config = utils[0]
+    site = utils[2]
+    dry_run = utils[4]
+    original_text = text
 
-     code = mwparserfromhell.parse(text)
-     for template in code.filter_templates():
-         if template.name.matches("nobots") or template.name.matches("Wikipedia:Exclusion compliant"):
-             if template.has("allow"):
-                 if "DeprecatedFixerBot" in template.get("allow").value:
-                     break # can edit
-             print("\n\nPage editing blocked as template preventing edit is present.\n\n")
-             return
+    code = mwparserfromhell.parse(text)
+    for template in code.filter_templates():
+        if template.name.matches("nobots") or template.name.matches("Wikipedia:Exclusion compliant"):
+            if template.has("allow"):
+                if "DeprecatedFixerBot" in template.get("allow").value:
+                    break  # can edit
+            print("\n\nPage editing blocked as template preventing edit is present.\n\n")
+            return
 
-     if not call_home(site):
+    if not call_home(site):
         raise ValueError("Kill switch on-wiki is false. Terminating program.")
-     edit_summary = 'Removed deprecated parameter(s) from [[Template:Track listing]] using' +  "[[User:" + config.get('enwikidep','username') + "| " + config.get('enwikidep','username') + "]]. Mistake? [[User talk:TheSandDoctor|msg TSD!]] (please mention that this is task #1!)"
-     time = 0
-     while True:
-         if time == 0:
-             text = page.text()
-         if time == 1:
-             page.purge()
-             original_text = site.Pages[page.page_title].text()
-         content_changed, text = remove_deprecated_params(original_text,dry_run)
-         try:
-             if dry_run:
-                 print("Dry run")
-                 #Write out the initial input
-                 text_file = open("DepInput01.txt", "w")
-                 text_file.write(original_text)
-                 text_file.close()
-                 #Write out the output
-                 if content_changed:
-                     text_file = open("DepOutput01.txt", "w")
-                     text_file.write(text)
-                     text_file.close()
-                 else:
-                     print("Content not changed, don't print output")
-                 break
-             else:
+    edit_summary = 'Removed deprecated parameter(s) from [[Template:Track listing]] using' + "[[User:" + config.get(
+        'enwikidep', 'username') + "| " + config.get('enwikidep',
+                                                     'username') + "]]. Mistake? [[User talk:TheSandDoctor|msg TSD!]] (please mention that this is task #1!)"
+    time = 0
+    while True:
+        if time == 0:
+            text = page.text()
+        if time == 1:
+            page.purge()
+            original_text = site.Pages[page.page_title].text()
+        content_changed, text = remove_deprecated_params(original_text, dry_run)
+        try:
+            if dry_run:
+                print("Dry run")
+                # Write out the initial input
+                text_file = open("DepInput01.txt", "w")
+                text_file.write(original_text)
+                text_file.close()
+                # Write out the output
+                if content_changed:
+                    text_file = open("DepOutput01.txt", "w")
+                    text_file.write(text)
+                    text_file.close()
+                else:
+                    print("Content not changed, don't print output")
+                break
+            else:
                 if verbose:
                     print("LIVE run")
                 if not content_changed:
@@ -97,17 +112,18 @@ def save_edit(page, utils, text):
                 if time == 1:
                     time = 0
                 break
-         except [[EditError]]:
-             print("Error")
-             time = 1
-             time.sleep(5)   # sleep for 5 seconds, giving server some time before querying again
-             continue
-         except [[ProtectedPageError]] as e:
-             print('Could not edit ' + page.page_title + ' due to protection')
-             print(e)
-         break
+        except [[EditError]]:
+            print("Error")
+            time = 1
+            time.sleep(5)  # sleep for 5 seconds, giving server some time before querying again
+            continue
+        except [[ProtectedPageError]] as e:
+            print('Could not edit ' + page.page_title + ' due to protection')
+            print(e)
+        break
 
-def remove_deprecated_params(text,dry_run):
+
+def remove_deprecated_params(text, dry_run):
     """
     Removes deprecated parameters from the {{Track listing}} template (and its redirects)
     @param text Page text to go over
@@ -119,31 +135,35 @@ def remove_deprecated_params(text,dry_run):
     templates = wikicode.filter_templates()
     content_changed = False
 
-    #TODO: Testing (dry run) only
+    # TODO: Testing (dry run) only
     if dry_run:
-        text_file = open("Input.txt","w")
+        text_file = open("Input.txt", "w")
         text_file.write(text)
         text_file.close()
-    #TODO: End dry run only
+    # TODO: End dry run only
 
     code = mwparserfromhell.parse(text)
     for template in code.filter_templates():
         if (template.name.matches("track listing") or template.name.matches("tracklisting")
-        or template.name.matches("tracklist") or template.name.matches("track") or template.name.matches("soundtrack")
-        or template.name.matches("tlist") or template.name.matches("track list") or template.name.matches("Track_listing")):
+                or template.name.matches("tracklist") or template.name.matches("track") or template.name.matches(
+                    "soundtrack")
+                or template.name.matches("tlist") or template.name.matches("track list") or template.name.matches(
+                    "Track_listing")):
             if template.has("writing_credits"):
-                template.remove("writing_credits",False)
+                template.remove("writing_credits", False)
                 content_changed = True
                 print("Removed writing_credits")
             if template.has("lyrics_credits"):
-                template.remove("lyrics_credits",False)
+                template.remove("lyrics_credits", False)
                 content_changed = True
                 print("Removed lyrics_credits")
             if template.has("music_credits"):
-                template.remove("music_credits",False)
+                template.remove("music_credits", False)
                 content_changed = True
                 print("Removed music_credits")
-    return [content_changed, str(code)] # get back text to save
+    return [content_changed, str(code)]  # get back text to save
+
+
 def main():
     limited_run = True
     pages_to_run = 500
@@ -155,9 +175,9 @@ def main():
     parser = argparse.ArgumentParser(prog='DeprecatedFixerBot Tracklisting fixer', description='''Goes through pages in the category [[:Category:Track listings with deprecated parameters]]
     and removes deprecated parameters.''')
     parser.add_argument("-dr", "--dryrun", help="perform a dry run (don't actually edit)",
-                    action="store_true")
-    parser.add_argument("-v","--verbose", help="Display more information when running",
-                    action="store_true")
+                        action="store_true")
+    parser.add_argument("-v", "--verbose", help="Display more information when running",
+                        action="store_true")
     args = parser.parse_args()
     if args.dryrun:
         dry_run = True
@@ -166,17 +186,16 @@ def main():
         print("Verbose mode")
         verbose = True
 
-    site = mwclient.Site(('https','en.wikipedia.org'), '/w/')
+    site = mwclient.Site(('https', 'en.wikipedia.org'), '/w/')
     config = configparser.RawConfigParser()
     config.read('credentials.txt')
     try:
-        site.login(config.get('enwikidep','username'), config.get('enwikidep', 'password'))
+        site.login(config.get('enwikidep', 'username'), config.get('enwikidep', 'password'))
     except errors.LoginError as e:
-        #print(e[1]['reason'])
         print(e)
         raise ValueError("Login failed.")
     counter = 0
-    utils = [config,None,site,False,dry_run]
+    utils = [config, None, site, False, dry_run]
     for page in site.Categories['Track listings with deprecated parameters']:
         if offset > 0:
             offset -= 1
@@ -196,6 +215,8 @@ def main():
             counter += 1
         else:
             return
+
+
 if __name__ == "__main__":
     try:
         verbose = False
